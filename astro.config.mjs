ead54bcfe6a5b50dae6c22e742d2617b83f27625
Preston TW <preston_tunnell_wilson@brown.edu>
2025-01-01 15:18:34 -0500

    Remove tailwind integration
    trying to minimize

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
-
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind()]
-});
+export default defineConfig({});
